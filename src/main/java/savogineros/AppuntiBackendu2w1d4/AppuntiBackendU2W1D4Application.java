4b3031948d11d7ac86060d1a5a790c18402145d3
Eros Savogin <eros.sav@gmail.com>
2024-01-11 21:33:34 +0100

    Initial commit

---
+++ src/main/java/savogineros/AppuntiBackendu2w1d4/AppuntiBackendU2W1D4Application.java
@@ -0,0 +1,13 @@
+package savogineros.AppuntiBackendu2w1d4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppuntiBackendU2W1D4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppuntiBackendU2W1D4Application.class, args);
+	}
+
+}
---
+++ src/test/java/savogineros/AppuntiBackendu2w1d4/AppuntiBackendU2W1D4ApplicationTests.java
@@ -0,0 +1,13 @@
+package savogineros.AppuntiBackendu2w1d4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppuntiBackendU2W1D4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
